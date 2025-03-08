03140f4e26bc43a266096ecf8b97ee4851370c7b
gauvain <gauvain.seigneur@gmail.com>
2025-03-08 12:41:21 +0100

    feat - handle gallery picking for ios

--- view/src/commonMain/kotlin/com.gauvain.seigneur/view/screens/AddScreen.kt
+++ view/src/commonMain/kotlin/com.gauvain.seigneur/view/screens/AddScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -169,5 +170,21 @@ private fun ColumnScope.TagSelectionBlock(screenState: ScreenStateUiModel<AddPub
             uiModels = uiModel.mood,
             modifier = Modifier.fillMaxWidth()
         )
+        Button(
+            onClick = {
+
+        },
+            modifier = Modifier
+                .padding(all = 32.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Publier",
+                style = MaterialTheme.typography.bodySmall,
+                modifier =
+                Modifier
+                    .padding(all = 8.dp)
+            )
+        }
     }
 }
